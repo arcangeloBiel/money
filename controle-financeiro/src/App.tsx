f72bd405374f70900f7e61f31541145c0cf02f56
joao ribeiro <joao.ribeiro@havan.com.br>
2022-01-17 15:51:59 -0300

    consumindo API

--- controle-financeiro/src/App.tsx
+++ controle-financeiro/src/App.tsx
@@ -1,12 +1,15 @@
+import { Dashboard } from "./components/Dashbord";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Ola Mundão</h1>
+    <>
+     <Header />
+     <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
